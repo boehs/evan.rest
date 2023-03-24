998a7216c5093c1928de1813c68c1522065fefa8
Evan Boehs <evan@boehs.org>
2023-03-24 10:07:57 -0400

    Small things

--- src/components/foot.tsx
+++ src/components/foot.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return <footer>
-    <p>By <a href="https://boehs.org">Evan Boehs</a> with love and rest. <a href="https://github.com/boehs/evan.rest">git</a></p>
+    <p>By <a href="https://boehs.org">Evan Boehs</a> with love and rest. <a href="https://github.com/boehs/evan.rest">Git</a>.</p>
   </footer>
 }
