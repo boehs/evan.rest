1be5de49f421bcc25cc35f72549f7a6e60bd67c1
Evan Boehs <evan@boehs.org>
2023-03-28 00:22:02 -0400

    theme colour

--- src/root.tsx
+++ src/root.tsx
@@ -22,6 +22,7 @@ export default function Root() {
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta name="theme-color" content="#e15be1"/>
         <Link rel="preconnect" href="https://fonts.googleapis.com" />
         <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <Link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
