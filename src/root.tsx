9fe806471b4938042f859dcaed6016afefd9cdac
Evan Boehs <evan@boehs.org>
2024-01-18 16:56:00 -0500

    add umami

--- src/root.tsx
+++ src/root.tsx
@@ -19,6 +19,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
+        <script async src="https://espy.boehs.org/script.js" data-website-id="c65e41eb-8bd1-4149-9087-a03846f6d103"></script>
         <Title>Evan.Rest</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
